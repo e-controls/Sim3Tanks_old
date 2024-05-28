86162539e97572241e975226ae8c5320771ca427
arllemfarias <arllem.f@gmail.com>
2024-05-28 16:47:30 -0400

    backup old Sim3Tanks

---
+++ three_tank_system/getters/get_k.m
@@ -0,0 +1,12 @@
+function [k] = get_k()
+% k = GET_K() returns the number of the current sample k.
+%
+% See also SET_K.
+
+% Written by Arllem Farias, 2018.
+
+global sample_k;
+
+k = sample_k;
+
+end
